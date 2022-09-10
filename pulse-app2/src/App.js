8db6cd4fa08f2e2957a7c72df391678afc174fda
Murthy K N <nmurthykoluvula@gmail.com>
2022-09-10 20:26:10 +0530

    added qualification and passing year

--- pulse-app2/src/App.js
+++ pulse-app2/src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Narasimha Murthy</h1>
-      <h2>Sample test</h2>
+      <h2>Qualification: B.Tech EEE</h2>
+      <h3>Year: 2012</h3>
     </div>
   );
 }
