c6cdba69ed2b9fe16ac76883324ab49c5341f429
Murthy K N <nmurthykoluvula@gmail.com>
2022-09-10 20:49:36 +0530

    reset the commit

--- pulse-app2/src/App.js
+++ pulse-app2/src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Narasimha Murthy</h1>
-      <h2>Qualification: B.Tech EEE</h2>
-      <h3>Year: 2012</h3>
+      <h1>Narasimha Murthy K</h1>
+      <h2>Qualification: B. Tech</h2>
     </div>
   );
 }
