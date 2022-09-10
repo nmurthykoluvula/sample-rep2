99727116b197d26d033ef02bacc67d6c06aa6133
Murthy K N <nmurthykoluvula@gmail.com>
2022-09-10 20:59:18 +0530

    deleted unwanted

--- pulse-app2/src/App.js
+++ pulse-app2/src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Narasimha Murthy K</h1>
-<<<<<<< HEAD
       <h2>Qualification: B. Tech</h2>
-=======
->>>>>>> 61942c4976c565cd939cffbe273e601c1e32327a
     </div>
   );
 }
